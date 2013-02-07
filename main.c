9ea99eb9c625c6587a0d3705fc513823bf2edb65
Liam Westby <lw322@nau.edu>
2013-02-06 18:40:17 -0700

    Initial commit.

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  raycast
+//
+//  Created by Liam Westby on 2/6/13.
+//  Copyright (c) 2013 Liam Westby. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
