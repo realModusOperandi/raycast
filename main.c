7716bd1940b8426634f186f12b16f970b6132a1b
Liam Westby <lw322@nau.edu>
2013-02-07 21:13:52 -0700

    Added main driver logic.

--- main.c
+++ main.c
@@ -7,12 +7,31 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "ppm.h"
+#include "vector.h"
+#include "sphere.h"
+#include "raycast.h"
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    char *output_path = "/Users/lw322/Documents/raycast.ppm";
+    ppm_image *the_image = (ppm_image*)malloc(sizeof(ppm_image));
+    the_image->height = 640;
+    the_image->width = 512;
+    the_image->maxval = 255;
+    the_image->data = (pixel**)malloc(sizeof(pixel*)*the_image->width);
+    for (int i = 0; i < the_image->width; i++) {
+        the_image->data[i] = (pixel*)malloc(sizeof(pixel)*the_image->height);
+        
+    }
+    float *origin = (float*)malloc(sizeof(float)*3);
+    v_init(0.0, 0.0, 0.4, origin);
+    sphere **objects = read_spheres(NULL);
+    raycast(the_image, objects, 6, the_image->width, the_image->height, 0.5, 0.4, origin, 0);
+    write_image(the_image, output_path);
     return 0;
 }
 
